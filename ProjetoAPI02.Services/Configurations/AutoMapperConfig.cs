438db289695ea2cdb4615816470c8ec73db91e13
smendescoti <sergio.coti@gmail.com>
2021-04-07 22:31:48 -0300

    Criado API para Ecommerce

---
+++ ProjetoAPI02.Services/Configurations/AutoMapperConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ProjetoAPI02.Domain.Entities;
+using ProjetoAPI02.Services.Models.Requests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoAPI02.Services.Configurations
+{
+    public class AutoMapperConfig : Profile
+    {
+        //construtor
+        public AutoMapperConfig()
+        {
+            //mapear as trocas de dados que deverão ser feitas
+            //atraves do AutoMapper <origem, destino>
+            CreateMap<ClientePostRequest, Cliente>();
+            CreateMap<EnderecoPostRequest, Endereco>();
+        }
+    }
+}
